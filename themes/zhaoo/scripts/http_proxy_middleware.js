85824b96405611f894883530913f1b67d68e9944
CherryWhite <1789139001@qq.com>
2022-09-18 10:51:32 +0800

    添加代理中间件

---
+++ themes/zhaoo/scripts/http_proxy_middleware.js
@@ -0,0 +1,14 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const apiProxy = createProxyMiddleware('/githubAgent', {
+    target: 'https://github.com/',
+    changeOrigin: true,
+    pathRewrite: {
+        '^/githubAgent/': '/', // rewrite path 将链接中的 /api/ 替换为 '/'
+    },
+});
+
+hexo.extend.filter.register('server_middleware', function(app){
+    // 表示以 agent 开头的请求将被转发
+    app.use('/githubAgent', apiProxy);
+});
