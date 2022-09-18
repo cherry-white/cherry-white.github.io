629c23831334fe57594498bb95cbd50af9064d90
CherryWhite <1789139001@qq.com>
2022-09-18 15:18:25 +0800

    天空之神

--- themes/zhaoo/scripts/http_proxy_middleware.js
+++ themes/zhaoo/scripts/http_proxy_middleware.js
@@ -3,12 +3,15 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 const apiProxy = createProxyMiddleware('/githubAgent', {
     target: 'https://github.com',
     changeOrigin: true,
+    protocolRewrite: 'https',
+    toProxy: true,
+    headers: { accept: "application/json" },
     pathRewrite: {
         '^/githubAgent/': '/', // rewrite path 将链接中的 /githubAgent/ 替换为 '/'
     },
 });
 
 hexo.extend.filter.register('server_middleware', function(app){
-    // 表示以 agent 开头的请求将被转发
+    // 表示以 githubAgent 开头的请求将被转发
     app.use('/githubAgent', apiProxy);
 });
